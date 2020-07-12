26e4e6855118b430c1814ac05a2ad3d889fba76e
ryota1116 <ryota1116funa@gmail.com>
2020-07-12 22:14:09 +0900

    [Add]料理のジャンルを選択すると、そのジャンルの料理のチェックボックスが表示される

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <v-layout wrap>
         <v-flex xs12 sm6 md4>
           <!-- xsが600px未満/ smが600px以上960px未満/ mdが960px以上1264px未満/ lgが1264px以上1904px未満/ xlが1904px以上 -->
-          <Home cuisines="cuisines"></Home>
+          <Home :french_collection="french_collection" :japanese_collection="japanese_collection"></Home>
         </v-flex>
       </v-layout>
     </v-container>
@@ -24,16 +24,22 @@ export default {
   // コンポーネントにdataを渡すときは、関数にしてreturnする必要がある
   data: function() {
     return {
-      cuisines: [
-        // type: Object
-      ]
+      french_collection: [],
+      japanese_collection: []
     }
   },
   created() {
-    firestore.collection("cuisine").get().then(querySnapshot => {
+    firestore.collection("french").get().then(querySnapshot => {
       querySnapshot.forEach(doc => {
         // doc.data() is never undefined for query doc snapshots
-        this.cuisines.push(doc.data());
+        this.french_collection.push(doc.data());
+        console.log(doc.data())
+      });
+    });
+    firestore.collection("japanese").get().then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+        // doc.data() is never undefined for query doc snapshots
+        this.japanese_collection.push(doc.data());
         console.log(doc.data())
       });
     });
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,13 +8,32 @@
           <h3>今回の設定金額: {{ selectedPrice }}円</h3>
           <br>
 
-          <!-- v-ifで表示するコンポーネントを切り替える -->
-          <v-select v-model="genre" :items="genres" label="メニュー"></v-select>
-          <component :is="selectedTabComponent"></component>
+          <!-- 料理のジャンル選択 -->
+          <v-select v-model="genre" :items="genres" label="ジャンル選択"></v-select>
 
-          <p>注文する料理</p>
-          <ul v-for="(cuisine, key) in orderedCuisine" :key="key">
-            <li>{{ key + 1 }}皿目: {{ cuisine.name }}</li>
+          <!-- 選択したジャンルの料理一覧 -->
+          <div v-for="(cuisine, index) in cuisinesOfSelectedGenre" :key="index">
+            <v-checkbox
+              v-model="listOfOrderedCuisines"
+              :value="cuisine"
+              :label="cuisine.name">
+            </v-checkbox>
+          </div>
+
+          <!-- <div v-for="(cuisine, index) in frenchCuisines" :key="index">
+            <input
+              type="checkbox"
+              :id="'cuisine' + key"
+              :value="{name: cuisine.name, price: cuisine.price}"
+              v-model="orderedCuisine"
+            >
+            <label :for="'cuisine' + key">{{ cuisine.name }}</label>
+          </div> -->
+
+          <!-- 選択した料理 -->
+          <h4>注文する料理</h4>
+          <ul v-for="(cuisine, index) in listOfOrderedCuisines" :key="index">
+            <li>{{ index + 1 }}皿目: {{ cuisine.name }}</li>
           </ul>
           <!-- 注文結果 -->
           <div class="my-2">
@@ -33,27 +52,35 @@
 </template>
 
 <script>
-import FrenchCuisine from './FrenchCuisine';
-import JapaneseCuisine from './JapaneseCuisine';
-import ItalianCuisine from './ItalianCuisine';
+// import FrenchCuisine from './FrenchCuisine';
+// import JapaneseCuisine from './JapaneseCuisine';
+// import ItalianCuisine from './ItalianCuisine';
 
 export default {
+  // ジャンル毎に取得したデータを複数propsで受け取る？
   props: {
-    cuisines: [],
+    french_collection: {
+      type: Array,
+      default: () => []
+    },
+    japanese_collection: {
+      type: Array,
+      default: () => []
+    }
   },
   components: {
-    FrenchCuisine,
-    JapaneseCuisine,
-    ItalianCuisine
+    // FrenchCuisine,
+    // JapaneseCuisine,
+    // ItalianCuisine
   },
   data: ()=> {
     return {
-      orderedCuisine: [],
       isActive: false,
       priceOptions: [5000, 10000, 25000],
       selectedPrice: "",
       genres: ['French', 'Japanese', 'Italian'],
       genre: "",
+      listOfOrderedCuisines: []
     }
   },
   methods:{
@@ -66,12 +93,30 @@ export default {
       return this.genre + "Cuisine";
       // .toLowerCase()
     },
+    cuisinesOfSelectedGenre: function() {
+      // 現状では、propsで受け取った複数のcollectionの中の1つのcollectionを格納してる
+      const cuisinesOfSelectedGenre = [];
+      if (this.genre === 'French') {
+        for (let i = 0; i < this.french_collection.length; i++) {
+          let cuisine = this.french_collection[i];
+          cuisinesOfSelectedGenre.push(cuisine);
+        }
+      } else if (this.genre === 'Japanese') {
+        for (let i = 0; i < this.japanese_collection.length; i++) {
+          let cuisine = this.japanese_collection[i];
+          cuisinesOfSelectedGenre.push(cuisine);
+        }
+      }
+      return cuisinesOfSelectedGenre;
+    },
     totalAmountMoneyOfOrder: function() {
-      const total = this.orderedCuisine.reduce((i, next) => i += Number(next.price), 0);
+      // 足し算してる
+      const total = this.listOfOrderedCuisines.reduce((i, next) => i += Number(next.price), 0);
       return total;
     },
     totalMoneyDeference: function() {
-      const total = this.orderedCuisine.reduce((i, next) => i += Number(next.price), 0);
+      // TODO:この定数totalをtotalAmountMoneyOfOrderと纏めれない？
+      const total = this.listOfOrderedCuisines.reduce((i, next) => i += Number(next.price), 0);
       const deference = total - this.selectedPrice;
       // const deference = Math.sign(total - 5000);
       return deference;
