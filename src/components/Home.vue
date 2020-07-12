15f5e87be117d47567e49a7a8ff2931d903cd58b
ryota1116 <ryota1116funa@gmail.com>
2020-07-12 14:00:54 +0900

    [Fix]料理ジャンル選択ボックスのUIを修正

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,12 +4,12 @@
       <v-row justify="center">
         <v-col cols="12">
           <h1>ゴチになります</h1>
-          <v-select v-model="selectedPrice" :items="options" label="設定金額"></v-select>
+          <v-select v-model="selectedPrice" :items="priceOptions" label="設定金額"></v-select>
           <h3>今回の設定金額: {{ selectedPrice }}円</h3>
           <br>
-          <h4>メニュー一覧</h4>
+
           <!-- v-ifで表示するコンポーネントを切り替える -->
-          <v-select v-model="currentTab" :items="genres" label="メニュー"></v-select>
+          <v-select v-model="genre" :items="genres" label="メニュー"></v-select>
           <component :is="selectedTabComponent"></component>
 
           <p>注文する料理</p>
@@ -48,20 +48,12 @@ export default {
   },
   data: ()=> {
     return {
-      orderedCuisine: [
-        // 下記はコメントアウトで良さそう
-        // {
-        //   name: '',
-        //   price: ''
-        // }
-      ],
+      orderedCuisine: [],
       isActive: false,
+      priceOptions: [5000, 10000, 25000],
       selectedPrice: "",
-      options: [5000, 10000, 25000],
-      cuisineGenre: "",
-
       genres: ['French', 'Japanese', 'Italian'],
-      currentTab: "",
+      genre: "",
     }
   },
   methods:{
@@ -71,7 +63,7 @@ export default {
   },
   computed: {
     selectedTabComponent: function() {
-      return this.currentTab + "Cuisine";
+      return this.genre + "Cuisine";
       // .toLowerCase()
     },
     totalAmountMoneyOfOrder: function() {
